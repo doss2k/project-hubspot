116cf12aeb4a296faac6ec03ca093a9bc18c7427
Jason Doss <doss2k@gmail.com>
2019-05-01 16:20:56 -0400

    Small changes for deployment.. maybe

--- server/server.js
+++ server/server.js
@@ -31,11 +31,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(mainRoutes);
 
 if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
-// Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static('client/build'));
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
