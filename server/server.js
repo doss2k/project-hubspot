a8c04dee5c8ab1b5252aa3556c653bb516eb1b88
Jason Doss <doss2k@gmail.com>
2019-05-01 15:13:32 -0400

    Roll back to prod env check on server

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ var cors = require("cors");
 
 var pool;
 
-if (true) {
+if (process.env.NODE_ENV === 'production') {
   // Express will serve up production assets
   // like our main.js file, or main.css file!
   app.use(express.static("client/build"));
