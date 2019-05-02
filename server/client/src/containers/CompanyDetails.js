e5adae6a781f0c5ad0edd3eb3cf1be13fc5f053f
marielklem <mariel.steinbeiser@gmail.com>
2019-05-02 09:25:13 -0400

    Updated company details to show

--- server/client/src/containers/CompanyDetails.js
+++ server/client/src/containers/CompanyDetails.js
@@ -3,14 +3,21 @@ import { Link } from "react-router-dom";
 
 export default class CompanyDetails extends Component {
   render() {
+    const { isActive, formClick } = this.props;
     return (
       <React.Fragment>
-        <div className="mask show" />
-        <div className="form-container show" style={{"position":"relative"}}>
-          <div className="form-header">Company Details 
-          <Link to="/"><i className="fas fa-times"></i></Link>
+        <div className={isActive ? "mask show" : "mask"} />
+          <div className={isActive ? "form-container show" : "form-container"}>
+            <div className={isActive ? "form-card show" : "form-card"}>
+              <div className="form-header-container">
+                <div className="form-header">
+                  <div className="form-name">company Details</div>
+                  <div className="fas fa-times" onClick={formClick} />
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
+
       </React.Fragment>
 
     )
