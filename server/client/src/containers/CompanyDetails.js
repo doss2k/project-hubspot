69efe7d29a47e387adf4a4c05bf8096396ac6f6c
marielklem <mariel.steinbeiser@gmail.com>
2019-05-02 12:46:01 -0400

    added edit button

--- server/client/src/containers/CompanyDetails.js
+++ server/client/src/containers/CompanyDetails.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
-import SubmitButton from "./SubmitButton";
 import { connect } from "react-redux";
 import * as actionTypes from "../actions";
 
 class CompanyDetails extends Component {
   onDelete(e) {
-    console.log('delete')
     this.props.deleteCompanyById(e)
     this.props.detailClick()
   }
@@ -25,19 +23,20 @@ class CompanyDetails extends Component {
               <div className="form-header-container">
                 <div className="form-header">
                   <div className="form-name">{companyName}</div>
+                  <div className="fas fa-pencil-alt" style={{'margin': "5px"}}/>
                   <div className="fas fa-times" onClick={detailClick} />
                 </div>
               </div>
               <div className="form-field-container">
-                <img src={logoUrl} style={{ 'width': '150px', 'height':'150px', 'overflow': 'hidden', 'borderRadius': '50%' }} />
+                <img src={logoUrl} alt="company logo"style={{ 'width': '150px', 'height':'150px', 'overflow': 'hidden', 'borderRadius': '50%' }} />
                 <p>{city}, {state} </p>
               </div>
               <div className="form-footer-container">
-              <input type="submit" value="delete" style={{'background': 'red', 'color': 'white'}} onClick={() => this.onDelete(companyId)}/>
-                <SubmitButton formClick={detailClick} />
+                <input type="submit" value="delete" style={{'background': 'red', 'color': 'white'}} onClick={() => this.onDelete(companyId)}/>
               </div>
             </div>
-            </div>
+          </div>
+          
 
       </React.Fragment>
 
