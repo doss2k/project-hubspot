6e99ee8f6288eb503247109bb641b44b1cd9924a
marielklem <mariel.steinbeiser@gmail.com>
2019-05-02 14:29:32 -0400

    Company Details rendering all deals

--- server/client/src/containers/CompanyDetails.js
+++ server/client/src/containers/CompanyDetails.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import { connect } from "react-redux";
 import * as actionTypes from "../actions";
 
+const formatter = new Intl.NumberFormat('en-us', {
+  style: 'currency',
+  currency: 'USD'
+});
+
 class CompanyDetails extends Component {
   onDelete(e) {
     this.props.deleteCompanyById(e)
@@ -13,12 +18,33 @@ class CompanyDetails extends Component {
 
   }
 
+  renderDeals() {
+    return this.props.company[1].map(deal => {
+      return (
+        <div
+          className="company-grid-row company-grid-items"
+          key={deal.dealId}
+        >
+          <div className="grid-title-items">
+            <div className="grid-logo-and-name-container">
+              <div className="text-link company-grid-name">
+                {deal.dealName}
+              </div>
+            </div>
+          </div>
+          <div className="grid-title-items">{deal.stage}</div>
+          <div className="grid-title-items">{formatter.format(deal.amount)}</div>
+        </div>
+      );
+    })
+  }
+
   render() {
     const { isActive, detailClick, formClick } = this.props;
     if (!this.props.company) {
       return <div>Please select a company</div>
     }
-    const { companyName, logoUrl, city, state, companyId } = this.props.company[0]
+    const { companyName, logoUrl, city, state, companyId } = this.props.company[0][0]
     return (
 
       <React.Fragment>
@@ -35,6 +61,16 @@ class CompanyDetails extends Component {
               <div className="form-field-container">
                 <img src={logoUrl} alt="company logo"style={{ 'width': '150px', 'height':'150px', 'overflow': 'hidden', 'borderRadius': '50%' }} />
                 <p>{city}, {state} </p>
+              <div>
+              <div className="company-grid-container">
+                <div className="company-grid-row company-grid-header">
+                  <div className="grid-title-items">deal</div>
+                  <div className="grid-title-items">stage</div>
+                  <div className="grid-title-items">amount</div>
+                </div>
+                {this.renderDeals()}
+                </div>  
+              </div>
               </div>
               <div className="form-footer-container">
                 <input type="submit" value="delete" style={{'background': 'red', 'color': 'white'}} onClick={() => this.onDelete(companyId)}/>
