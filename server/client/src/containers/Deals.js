3c343fdb6324f2ca4b6d4a1d4cab764c9fd6c0a1
RITCHIE FESPERMAN <ritchiefesperman@RITCHIEs-MacBook-Pro.local>
2019-05-03 00:53:03 -0400

    improve body and background styling

--- server/client/src/containers/Deals.js
+++ server/client/src/containers/Deals.js
@@ -1,19 +1,30 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import React, { Component } from "react";
+import { connect } from "react-redux";
 // import dnddata from '../static/sampleData/dnddata'
-import StageColumn from './StageColumn'
-import { DragDropContext } from 'react-beautiful-dnd'
-import Button from './Button'
-import DealsForm from './DealsForm'
-import { bindActionCreators } from 'redux';
+import StageColumn from "./StageColumn";
+import { DragDropContext } from "react-beautiful-dnd";
+import Button from "./Button";
+import DealsForm from "./DealsForm";
+import { bindActionCreators } from "redux";
 
-import { getAllDeals, getDealPosition, setDealPosition, getAllCompanies } from '../actions/'
+import {
+  getAllDeals,
+  getDealPosition,
+  setDealPosition,
+  getAllCompanies
+} from "../actions/";
 
 export class Deals extends Component {
   state = {
-    stageOrder: ['Initiated', 'Qualified', 'Contract Sent', 'Closed Won', 'Closed Lost'],
+    stageOrder: [
+      "Initiated",
+      "Qualified",
+      "Contract Sent",
+      "Closed Won",
+      "Closed Lost"
+    ],
     showForm: false
-  }
+  };
 
   componentDidMount() {
     this.props.getAllDeals();
@@ -22,36 +33,36 @@ export class Deals extends Component {
   }
 
   formClick = () => {
-    this.setState({ showForm: !this.state.showForm })
-  }
+    this.setState({ showForm: !this.state.showForm });
+  };
 
   onDragEnd = result => {
-    const { destination, source, draggableId } = result
+    const { destination, source, draggableId } = result;
 
     if (!destination) {
-      return
+      return;
     }
 
     if (
       destination.droppableId === source.droppableId &&
       destination.index === source.index
     ) {
-      return
+      return;
     }
 
-    const startingStage = this.props.stages[source.droppableId]
-    const endingStage = this.props.stages[destination.droppableId]
+    const startingStage = this.props.stages[source.droppableId];
+    const endingStage = this.props.stages[destination.droppableId];
 
     if (startingStage === endingStage) {
-      const newDealIds = [...startingStage.dealId]
+      const newDealIds = [...startingStage.dealId];
 
-      newDealIds.splice(source.index, 1)
-      newDealIds.splice(destination.index, 0, draggableId)
+      newDealIds.splice(source.index, 1);
+      newDealIds.splice(destination.index, 0, draggableId);
 
       const newStage = {
         ...startingStage,
         dealId: newDealIds
-      }
+      };
 
       const newState = {
         ...this.props,
@@ -59,25 +70,25 @@ export class Deals extends Component {
           ...this.props.stages,
           [newStage.id]: newStage
         }
-      }
+      };
 
       this.props.setDealPosition(newState);
       return;
     }
 
-    const startDealIds = [...startingStage.dealId]
-    startDealIds.splice(source.index, 1)
+    const startDealIds = [...startingStage.dealId];
+    startDealIds.splice(source.index, 1);
     const newStartingStage = {
       ...startingStage,
       dealId: startDealIds
-    }
+    };
 
-    const finishDealIds = [...endingStage.dealId]
-    finishDealIds.splice(destination.index, 0, draggableId)
+    const finishDealIds = [...endingStage.dealId];
+    finishDealIds.splice(destination.index, 0, draggableId);
     const newEndingStage = {
       ...endingStage,
       dealId: finishDealIds
-    }
+    };
 
     const newState = {
       ...this.props,
@@ -86,31 +97,37 @@ export class Deals extends Component {
         [newStartingStage.id]: newStartingStage,
         [newEndingStage.id]: newEndingStage
       }
-    }
+    };
 
     this.props.setDealPosition(newState);
-  }
+  };
 
   render() {
     if (this.props.stages && this.props.deals) {
       return (
         <React.Fragment>
+          <div className="background-layer" />
+          <div className="background-highlight-layer" />
           <div className="header-div">
             <h2>Deals</h2>
-            <Button title={'Create Deal'} formClick={this.formClick} />
+            <Button title={"Create Deal"} formClick={this.formClick} />
           </div>
           {/* comment this out if you don't want to see it */}
-          <DealsForm isActive={this.state.showForm} formClick={this.formClick} companies={this.props.companies}/>
+          <DealsForm
+            isActive={this.state.showForm}
+            formClick={this.formClick}
+            companies={this.props.companies}
+          />
           <DragDropContext onDragEnd={this.onDragEnd}>
             <div className="deal-grid-container">
               {this.state.stageOrder.map(stageId => {
-                const stage = this.props.stages[stageId]
+                const stage = this.props.stages[stageId];
                 const deals = stage.dealId.map(
                   dealId => this.props.deals[dealId]
-                )
+                );
                 const amount = deals.reduce((total, deal) => {
-                  return (total += deal.amount)
-                }, 0)
+                  return (total += deal.amount);
+                }, 0);
                 return (
                   <StageColumn
                     key={stage.id}
@@ -118,14 +135,14 @@ export class Deals extends Component {
                     deals={deals}
                     amount={amount}
                   />
-                )
+                );
               })}
             </div>
           </DragDropContext>
         </React.Fragment>
-      )
+      );
     } else {
-      return <div>Loading...</div>
+      return <div>Loading...</div>;
     }
   }
 }
@@ -135,16 +152,22 @@ const mapStateToProps = state => {
     deals: state.dealsReducer.deals,
     stages: state.dealsReducer.stages,
     companies: state.companiesReducer.companies
-  }
-}
+  };
+};
 
 const mapDispatchToProps = dispatch => {
-  return bindActionCreators({ getAllDeals, getAllCompanies, getDealPosition, setDealPosition }, dispatch)
+  return bindActionCreators(
+    { getAllDeals, getAllCompanies, getDealPosition, setDealPosition },
+    dispatch
+  );
   // return {
   //   getAllDeals: () => dispatch(actionTypes.getAllDeals()),
   //   getDealPosition: () => dispatch(actionTypes.getDealPosition()),
   //   setDealPosition: newState => dispatch(actionTypes.setDealPosition(newState))
   // }
-}
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(Deals);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Deals);
