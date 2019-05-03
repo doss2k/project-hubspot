d3eb7bdbc22a96cb59dd0d60cae52ba038f36a1f
Luc Saunders <lucsaunders@gmail.com>
2019-05-03 13:08:42 -0400

    Reorganize Infographics into separate card components.

--- server/client/src/components/dashboard/Infographics/TimeToClose.js
+++ server/client/src/components/dashboard/Infographics/TimeToClose.js
@@ -1,12 +1,15 @@
-import React, { Component } from "react";
-import numeral from "numeral";
+import React, { Component } from 'react';
+import numeral from 'numeral';
 
 export class TimeToClose extends Component {
   render() {
     const { data } = this.props;
     return (
       <div className="avgTimeToClose info-card">
-        <p className="variable-info-p">{numeral(data).format("0,0")}</p>
+        <p className="variable-info-p">
+          {numeral(data / 1000 / 60 / 60 / 24).format('0,0')}
+          <span className="percentage"> days</span>
+        </p>
         <h4>Average time to close deal</h4>
       </div>
     );
